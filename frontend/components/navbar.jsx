87c7bb672c80d42a6b963b4b59bab1e74039b48d
Andrew Hwang <pandy430@gmail.com>
2024-04-17 01:07:24 -0700

    style-navbar

---
+++ frontend/components/navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+        <div className="navbarPart">
+            <a href="/">
+                this
+            </a>
+            <a href="/">
+                is a
+            </a>
+        </div>
+        <div className="navbarPart">
+            <a href="/">
+                fake
+            </a>
+            <a href="/">
+                navbar
+            </a>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import axios from "axios";
 import React, { useEffect, useState} from "react";
 import Quotes from "../components/quote";
+import Navbar from "../components/navbar";
 
 function App() {
 	const [quotesData, setQuotesData] = useState({});
@@ -9,7 +10,6 @@ function App() {
         const getData = async () => {
             const quotes = await axios.get('/api/quote');
             setQuotesData(quotes.data);
-			console.log(quotesData);
         };
         getData();
     }, []);
@@ -18,25 +18,11 @@ function App() {
 	return (
 		<div className="App">
 			{/* TODO: include an icon for the quote book */}
-			<h1>Hack at UCI Tech Deliverable</h1>
-
-			<h2>Submit a quote</h2>
-			{/* TODO: implement custom form submission logic to not refresh the page */}
-			<form action="/api/quote" method="post">
-				<label htmlFor="input-name">Name</label>
-				<input type="text" name="name" id="input-name" required />
-				<label htmlFor="input-message">Quote</label>
-				<input type="text" name="message" id="input-message" required />
-				<button type="submit">Submit</button>
-			</form>
-
-			<h2>Previous Quotes</h2>
-			{/* TODO: Display the actual quotes from the database */}
-			<div className="messages">
-				{Object.entries(quotesData).map(([key, value]) => (
-    				<Quotes key={key} singleQuote={value} />
-				))}
+			<Navbar></Navbar>
+			<div className="Inside">
+			hi
 			</div>
+			
 		</div>
 	);
 }
