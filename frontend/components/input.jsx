80afb7f39bf1dc9128debbbce0f8f1b229755dc7
Andrew Hwang <pandy430@gmail.com>
2024-04-17 03:22:05 -0700

    complete-input-style

--- frontend/components/input.jsx
+++ frontend/components/input.jsx
@@ -4,12 +4,31 @@ export const Input = () => {
   return (
     <div className="input">
         <div className="input-text-area">
-            <div className="input-text">
-                send a quote
+            <div className="input-name">
+                <div className="input-name-text">name:</div>
+                <div className="input-name-field" />
             </div>
-            <div className="input-under-text">
-                leave your mark on this world. change someones life
+            <div className="input-description">
+                <div className="input-description-text">quote:
+                <div className='input-description-push'>
+
+                </div>
+                </div>
+                <div className="input-description-field" />
             </div>
+        </div>
+        <div className="input-text-area">
+            <div>
+                <div className="input-text">
+                    send a quote
+                </div>
+                <div className="input-under-text">
+                    leave your mark on this world. change someones life
+                </div>
+            </div>
+            <div className="input-submit">
+                    submit
+                </div>
         </div>
     </div>
   )
